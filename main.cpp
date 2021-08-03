8a49bb4dd4bcafc66301cabc8bf3246fe520fe7b
Javier Valdez <galatar14v@gmail.com>
2021-08-02 20:44:32 -0600

    Creando menu

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	
+	int opcion;
+	
+	cout << "******** Menu ********"<<endl;
+	cout << " 1. Carga de usuarios"<<endl;
+	cout << " 2. Carga de tareas"<<endl;
+	cout << " 3. Ingreso manual"<<endl;
+	cout << " 4. Reportes"<<endl;
+	cout << "ingrese el numero de opcion"<<endl;
+	cin>>opcion;
+	
+	return 0;		
+}
